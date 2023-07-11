764db0aad0aa0b4fa88d47ea9956314147e432bb
Vince-lgd <lagardevincent64@gmail.com>
2023-07-11 16:40:01 +0200

    save

--- PortFolioV2/src/App.vue
+++ PortFolioV2/src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <NavBar />
+    <div>
+      <NavBar />
+    </div>
+
+    <div class="title">
+      <h1>Vincent Lagarde</h1>
+      <h2>Développeur Web</h2>
+    </div>
+    <div class="stars">
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+      <div class="star"></div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +23,43 @@
 import NavBar from "./components/NavBar.vue";
 </script>
 
-<style></style>
+<style>
+body {
+  background-color: rgb(20, 89, 91);
+}
+.title {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4rem;
+}
+h1 {
+  font-size: 3rem;
+}
+h2 {
+  font-size: 1.75rem;
+}
+.stars {
+  display: flex;
+  gap: 8rem;
+}
+.star {
+  background-color: rgb(255, 255, 201);
+  backdrop-filter: blur(10px);
+  box-shadow: 0px 0px 20px 10px #a3b4d5;
+  border: 1px solid rgb(255, 255, 201);
+  border-radius: 10rem;
+  width: 5px;
+  height: 5px;
+  animation-name: clignot;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes clignot {
+  100% {
+    transform: translateY(1200px);
+  }
+}
+</style>
--- PortFolioV2/src/components/NavBar.vue
+++ PortFolioV2/src/components/NavBar.vue
@@ -1,21 +1,48 @@
 <template>
-  <div>
+  <div class="body">
     <div class="menu">
       <router-link to="/home">
-        <button>Home</button>
+        <div class="but"><p>Home</p></div>
       </router-link>
-      <h1>Présentation</h1>
-      <h1>Compétences</h1>
-      <h1>Contact</h1>
+      <router-link to="/profil">
+        <div class="but"><p>Présentation</p></div>
+      </router-link>
+      <router-link to="/projects">
+        <div class="but"><p>Compétences / Projets</p></div>
+      </router-link>
+      <router-link to="/contact">
+        <div class="but"><p>Contact</p></div>
+      </router-link>
     </div>
+
     <router-view></router-view>
   </div>
 </template>
 
-<script setup></script>
+<script></script>
 
-<style>
+<style scoped>
+.body {
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+}
 .menu {
   display: flex;
+  gap: 5rem;
+  text-decoration: none;
+}
+.but {
+  display: flex;
+  justify-content: center;
+  border: solid 1px red;
+  width: 8rem;
+  border-radius: 20px;
+  text-decoration: none;
+  cursor: pointer;
+}
+p {
+  text-decoration: none;
+  color: black;
 }
 </style>
