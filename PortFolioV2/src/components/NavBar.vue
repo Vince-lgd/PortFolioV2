32ece5a0d53045c514fbc85715523ddcf64d5cba
Vince-lgd <lagardevincent64@gmail.com>
2024-05-21 15:09:30 +0200

    change title navbar

--- PortFolioV2/src/components/NavBar.vue
+++ PortFolioV2/src/components/NavBar.vue
@@ -10,7 +10,7 @@
       <div ref="circle" class="freelance">
         <div ref="circle" class="circle"></div>
         <a ref="changeColor" href="mailto:lagardevincent64@gmail.com">
-          <h2>Recherche une entreprise pour une alternance</h2>
+          <h2>Portfolio</h2>
         </a>
       </div>
       <div ref="texte" class="but">
@@ -84,7 +84,7 @@ onMounted(() => {
   border: solid 1px aliceblue;
   border-radius: 20px;
   height: 2rem;
-  width: 25%;
+  width: 10%;
   gap: 5px;
 }
 
